1beb51d81c6a1f99e160f708ea3928b2ed1119c1
RudroMondal <rudromondal151@gmail.com>
2026-02-07 00:30:31 +0600

    Dictionary
    Basic of Dictionary

---
+++ Dictionary_in_python.py
@@ -0,0 +1,36 @@
+# Syntax of dictionary:
+name_age={"Rudro":20,"Protik":19} #'Rudro and Protik' are key, '20 and 19' are value
+
+# Empty Dictionary
+Name_Age={}
+
+# How to add a element in dictionary:
+Name_Age["Surjo"]=18
+Name_Age["Turjo"]=19
+Name_Age["Babu"]=17
+Name_Age["Arko"]=21
+
+# How to remove a element in dictionary:
+del Name_Age["Babu"]
+
+# How to aecess key
+NAME=Name_Age["Surjo"]
+
+# How to update value
+Name_Age["Arko"]=20
+
+# How to clear dictionary
+name_age.clear()
+
+# How to know length of a dictionary
+# print(len(Name_Age))
+
+# aecess key
+# print(NAME)
+
+# print name_age
+# print(name_age)
+
+# print Name_Age
+print(Name_Age)
+
